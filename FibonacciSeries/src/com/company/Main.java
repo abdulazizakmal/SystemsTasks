cd86d58815d40923d2740abd98b1205dff7bed67
abdulazizakmal <49819149+abdulazizakmal@users.noreply.github.com>
2021-07-23 15:32:25 +0500

    Update Main.java

--- FibonacciSeries/src/com/company/Main.java
+++ FibonacciSeries/src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+/* Printing first 10 numbers of fibonacci series using Stack
+*/
+
 import java.util.Stack;
 
 public class Main {
@@ -10,6 +13,8 @@ public class Main {
         System.out.println(fibonacci.peek());
         fibonacci.push(1);
         System.out.println(fibonacci.peek());
+        
+        // using for loop to print the series
         for(int i=0;i<10;i++)
         {
             int temp = fibonacci.pop();
@@ -18,11 +23,4 @@ public class Main {
             fibonacci.push(temp);
             System.out.println(fibonacci.peek());
         }    }
-    int n=5;
-    int j=0;
-    int temp = 1;
-    for(int i=0;j<n;i=i+temp)
-    {
-        
-    }
 }
